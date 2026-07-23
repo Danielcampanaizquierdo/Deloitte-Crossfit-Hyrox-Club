44b1b0933f790006c42c9ae2ca50df27f995717a
Danielcampanaizquierdo <danielcampanaizquierdo@gmail.com>
2026-07-23 12:27:41 +0200

    Update _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,3 +1,8 @@
+<link rel="icon" href="/favicon.ico" sizes="any" />
+<link rel="icon" type="image/png" href="/favicon-32.png" />
+<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+<link rel="manifest" href="/manifest.json" />
+<meta name="theme-color" content="#000000" />
 /** @jsx h */
 /** @jsxFrag Fragment */
 import { h, Fragment } from "preact";
