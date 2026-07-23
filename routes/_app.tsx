6b8faee3c3b4cd5e9a1237647cb1db19cc590dc5
Danielcampanaizquierdo <danielcampanaizquierdo@gmail.com>
2026-07-23 12:50:55 +0200

    Update _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,10 +7,6 @@ export default function App({ Component }: AppProps) {
   return (
     <>
       <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover"
-        />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" type="image/png" href="/favicon-32.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
