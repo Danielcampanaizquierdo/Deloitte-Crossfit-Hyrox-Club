f23de5e7f21df44e112fd44a9db3e9eea8964584
Danielcampanaizquierdo <danielcampanaizquierdo@gmail.com>
2026-07-23 12:53:40 +0200

    Update _app.tsx BUENO

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,5 +1,6 @@
 /** @jsx h */
 /** @jsxFrag Fragment */
+import { h, Fragment } from "preact";
 import { AppProps } from "$fresh/server.ts";
 import { Head } from "$fresh/runtime.ts";
 
@@ -7,6 +8,10 @@ export default function App({ Component }: AppProps) {
   return (
     <>
       <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" type="image/png" href="/favicon-32.png" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
