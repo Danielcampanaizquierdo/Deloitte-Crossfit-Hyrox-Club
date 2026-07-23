c45412651b992bc4ef41123b7e1ae7154e1ae22f
Danielcampanaizquierdo <danielcampanaizquierdo@gmail.com>
2026-07-23 12:37:34 +0200

    Update _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,16 +1,23 @@
-<link rel="icon" href="/favicon.ico" sizes="any" />
-<link rel="icon" type="image/png" href="/favicon-32.png" />
-<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-<link rel="manifest" href="/manifest.json" />
-<meta name="theme-color" content="#000000" />
 /** @jsx h */
 /** @jsxFrag Fragment */
 import { h, Fragment } from "preact";
 import { AppProps } from "$fresh/server.ts";
+import { Head } from "$fresh/runtime.ts";
 
 export default function App({ Component }: AppProps) {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" href="/favicon-32.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
       <Component />
     </>
   );
